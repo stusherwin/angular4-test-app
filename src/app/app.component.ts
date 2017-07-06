633469acea0a22b53dbe5aa3c46652d0353936ef
Stu Sherwin <stu.sherwin@gmail.com>
2017-07-06 17:55:54 +0100

    Read only when in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/delay'
 import 'rxjs/add/operator/materialize'
 import 'rxjs/add/operator/dematerialize'
 import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/filter'
 import { Subject } from 'rxjs/Subject'
 
 export class ApiAddWidgetRequest {
@@ -76,34 +77,92 @@ export class WidgetService {
   }
 }
 
-export class ActionModel {
+export interface IKeyed {
+  key: string
+}
+
+export interface IReadOnly {
+  isReadOnly: boolean
+}
+
+export class ReadOnlyService implements IReadOnly {
+  private _isReadOnly = false
+
+  get isReadOnly() { return this._isReadOnly }
+
+  readOnly() {
+    this._isReadOnly = true
+  }
+
+  readWrite() {
+    this._isReadOnly = false
+  }
+}
+
+export class EditService {
+  private _currentlyEditingKey: string
+  private _currentlyEditingSubject = new Subject<string>()
+  
+  startEdit(item: IKeyed) {
+    this._currentlyEditingKey = item.key
+    this._currentlyEditingSubject.next(item.key)
+  }
+
+  endEdit(item: IKeyed) {
+    if(this._currentlyEditingKey == item.key) {
+      this._currentlyEditingKey = null
+      this._currentlyEditingSubject.next(null)
+    }
+  }
+
+  onEndEdit(item: IKeyed, action: () => void) {
+    this._currentlyEditingSubject.filter(k => k != item.key).subscribe(action)
+  }
+}
+
+export class ActionModel implements IKeyed, IReadOnly {
   status: string = ''
   errorMessage: string = ''
 
   constructor(
+    public key: string,
+    protected _editService: EditService,
+    protected _readOnlyService: ReadOnlyService,
     private _confirm: () => Observable<any>,
     private _done: () => void,
     private _abandon: () => void
   ) {
+    this._editService.onEndEdit(this, () => {
+      if(this.isError) {
+        this.abandon()
+      }
+    })
   }
 
   get isInProgress() {
-    return this.status && this.status == 'inProgress'
+    return this.status == 'inProgress'
   }
 
   get isSuccess() {
-    return this.status && this.status == 'success' || this.status == 'done'
+    return this.status == 'success' || this.status == 'done'
   }
 
   get isError() {
-    return this.status && this.status == 'error'
+    return this.status == 'error'
   }
 
   get isDone() {
-    return this.status && this.status == 'done'
+    return this.status == 'done'
+  }
+
+  get isReadOnly() {
+    return this._readOnlyService.isReadOnly
   }
 
   confirm() {
+    this._editService.startEdit(this)
+    this._readOnlyService.readOnly()
+
     this.status = 'inProgress'
     this._confirm().subscribe(
       r => {
@@ -127,6 +186,7 @@ export class ActionModel {
   private error(errorMessage: string) {
     this.status = 'error'
     this.errorMessage = errorMessage
+    this._readOnlyService.readWrite()    
   }
 
   private done() {
@@ -137,44 +197,51 @@ export class ActionModel {
   protected clear() {
     this.status = null
     this.errorMessage = null
+    this._editService.endEdit(this)
+    this._readOnlyService.readWrite()    
   }
 
-  retry() {
+  protected retry() {
     this.confirm()
   }
 
-  abandon() {
+  protected abandon() {
     this.clear()
     this._abandon()
   }
 }
 
 export class EditModel extends ActionModel {
-  editing: boolean
+  editing = false
 
   constructor(
+    key: string,
+    editService: EditService,
+    readOnlyService: ReadOnlyService,
     _confirm: () => Observable<any>,
     _done: () => void,
     _abandon: () => void,
   ) {
-    super(_confirm, _done, _abandon)
+    super(key, editService, readOnlyService, _confirm, _done, _abandon)
+    this._editService.onEndEdit(this, () => {
+      if(this.editing) {
+        this.cancel() 
+      }
+    })
   }
 
   start() {
     this.editing = true
+    this._editService.startEdit(this)
   }
 
   cancel() {
+    this.editing = false
     this.clear()
   }
 
   protected success() {
     super.success()
-  }
-
-  protected clear() {
-    super.clear()
-    this.editing = false
   }
 }
 
@@ -182,12 +249,15 @@ export class TextEditModel extends EditModel {
   editingValue: string
 
   constructor(
+    key: string,
+    editService: EditService,
+    readOnlyService: ReadOnlyService,
     _confirm: () => Observable<any>,
     _done: () => void,
     _abandon: () => void,
     public value: string
   ) {
-    super(_confirm, _done, _abandon)
+    super(key, editService, readOnlyService, _confirm, _done, _abandon)
     this.editingValue = this.value
   }
 
@@ -198,7 +268,6 @@ export class TextEditModel extends EditModel {
 
   protected clear() {
     super.clear()
-    this.editing = false
     this.editingValue = this.value
   }
 }
@@ -207,12 +276,15 @@ export class PriceEditModel extends EditModel {
   editingValue: string
 
   constructor(
+    key: string,
+    editService: EditService,
+    readOnlyService: ReadOnlyService,
     _confirm: () => Observable<any>,
     _done: () => void,
     _abandon: () => void,
     public value: number
   ) {
-    super(_confirm, _done, _abandon)
+    super(key, editService, readOnlyService, _confirm, _done, _abandon)
     this.editingValue = this.value.toFixed(2)
   }
 
@@ -223,40 +295,59 @@ export class PriceEditModel extends EditModel {
 
   protected clear() {
     super.clear()
-    this.editing = false
     this.editingValue = this.value.toFixed(2)
   }
 }
 
-export class WidgetModel {
+export class WidgetModel implements IReadOnly {
   add: ActionModel
   delete: ActionModel
   name: TextEditModel
   price: PriceEditModel
+  _readOnlyService: ReadOnlyService
+
+  get isReadOnly() {
+    return this._readOnlyService.isReadOnly
+  }
 
   constructor(
     public id: number,
     name: string,
     price: number,
     private _service: WidgetService,
+    private _editService: EditService,
     private _parent: WidgetsModel) {
+      this._readOnlyService = new ReadOnlyService()
+
       this.add = new ActionModel(
+        'add:' + this.id,
+        this._editService,
+        this._readOnlyService,
         () => this.confirmAdd(),
         () => {},
         () => this._parent.abandonAdd(this))
 
       this.delete = new ActionModel(
+        'delete:' + this.id,
+        this._editService,
+        this._readOnlyService,
         () => this.confirmDelete(),
         () => this._parent.deleteDone(this),
         () => {})
 
       this.name = new TextEditModel(
+        'name:' + this.id,
+        this._editService,
+        this._readOnlyService,
         () => this.confirmEditName(),
         () => {},
         () => {},
         name)
 
       this.price = new PriceEditModel(
+        'price:' + this.id,
+        this._editService,
+        this._readOnlyService,
         () => this.confirmEditPrice(),
         () => {},
         () => {},
@@ -286,29 +377,38 @@ export class AddingWidgetModel {
   price = ''
 }
 
-export class WidgetsModel {
+export class WidgetsModel implements IKeyed {
   loaded = false
   adding: AddingWidgetModel
   widgets: WidgetModel[] = []
   editing: WidgetModel
   editingField: string
+  key = 'add'
+
+  _editService = new EditService()
 
   constructor(private _service: WidgetService) {
+    this._editService.onEndEdit(this, () => {
+      if(this.adding) {
+        this.cancelAdd()
+      }
+    })
   }
 
   load() {
     this._service.all().subscribe(widgets => {
-      this.widgets = widgets.map(w => new WidgetModel(w.id, w.name, w.price, this._service, this))
+      this.widgets = widgets.map(w => new WidgetModel(w.id, w.name, w.price, this._service, this._editService, this))
       this.loaded = true
     })
   }
 
   startAdd() {
     this.adding = new AddingWidgetModel()
+    this._editService.startEdit(this)
   }
 
   confirmAdd() {
-    let widget = new WidgetModel(0, this.adding.name, parseFloat(this.adding.price), this._service, this)
+    let widget = new WidgetModel(0, this.adding.name, parseFloat(this.adding.price), this._service, this._editService, this)
     this.adding = null
     this.widgets.unshift(widget)
     widget.add.confirm()
@@ -321,6 +421,7 @@ export class WidgetsModel {
 
   cancelAdd() {
     this.adding = null
+    this._editService.endEdit(this)
   }
 
   deleteDone(widget: WidgetModel) {
